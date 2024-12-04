086a757657a8db5b6d85de79602e3919dfdfcd34
Raul Garcia <raul.garcia@factoriaf5.org>
2024-12-04 10:14:57 +0100

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: "./src",
+    base: "/test-rutas/",
+    build: {
+        outDir: "../docs",
+        emptyOutDir: true,
+
+    }
+});
