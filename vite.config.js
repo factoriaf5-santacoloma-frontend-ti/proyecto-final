09051b5627b063429122895c364ee83417c2e021
Raul Garcia <raul.garcia@factoriaf5.org>
2024-12-09 11:35:48 +0100

    add deploying configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,21 @@ export default defineConfig({
     build: {
         outDir: "../docs",
         emptyOutDir: true,
+        rollupOptions:{
+            input:{
+            index: "/index.html",
+            sala_alba:"/views/sala_alba.html",
+            sala_deep:"/views/sala_deep.html",
+            sala_elsa:"/views/sala_elsa.html",
+            sala_faryal:"/views/sala_faryal.html",
+            sala_hassan:"/views/sala_hassan.html",
+            sala_jiale:"/views/sala_jiale.html",
+            sala_jorge:"/views/sala_jorge.html",
+            sala_juan:"/views/sala_juan.html",
+            sala_luisa:"/views/sala_luisa.html",
+            sala_moha:"/views/sala_moha.html",
+            sala_paola:"/views/sala_paola.html",
+        }}
 
     }
 });
