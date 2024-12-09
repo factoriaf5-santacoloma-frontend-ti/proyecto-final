b1205b85bd70a2c2d448fb3843ebaeadaff49146
Paola <paolla.oyaneder@gmail.com>
2024-12-09 14:13:11 +0100

    lola falta ñ

--- src/js/sala_paola.js
+++ src/js/sala_paola.js
@@ -11,4 +11,23 @@ document.querySelector('a-scene').addEventListener('loaded', function () {
       infoContainer.setAttribute('visible', isInfoVisible.toString()); // Convertir a cadena 'true' o 'false'
     });
   });
-  
+
+
+  AFRAME.registerComponent('toggle-text', {
+    schema: {
+      groupId: { type: 'selector' },
+    },
+    init: function () {
+      const el = this.el;
+      const groupEl = this.data.groupId;
+      el.addEventListener('click', () => {
+        if (groupEl) {
+          const isVisible = groupEl.getAttribute('visible') === true || groupEl.getAttribute('visible') === "true";
+          groupEl.setAttribute('visible', !isVisible);
+          console.log(`Grupo ahora es ${!isVisible ? 'visible' : 'oculto'}`);
+        } else {
+          console.error('No se encontró el grupo asociado.');
+        }
+      });
+    },
+  });
