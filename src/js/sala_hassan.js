cf778d031757fdc06f92ea4fbb4b97c90163a299
hassanbarag <hassanbarag100@gmail.com>
2024-12-10 12:46:24 +0100

    camera

--- src/js/sala_hassan.js
+++ src/js/sala_hassan.js
@@ -19,3 +19,18 @@ AFRAME.registerComponent("play",{
             });
         }
         });
+
+        const cameraRig = document.querySelector("#cameraRig");
+
+        // Escuchar eventos de teclado
+        document.addEventListener("keydown", (event) => {
+          const step = 0.5; // Velocidad de movimiento vertical
+          const position = cameraRig.object3D.position;
+    
+          // Movimiento vertical con Q y E
+          if (event.key === "e") {
+            position.y += step; // Subir
+          } else if (event.key === "q") {
+            position.y -= step; // Bajar
+          }
+        });
