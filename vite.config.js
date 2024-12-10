01abe9976900b30a185cac6af18e561525a3cf1c
Raul Garcia <raul.garcia@factoriaf5.org>
2024-12-10 11:23:29 +0100

    solve vite.config.js merge conflilct

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,17 @@ export default defineConfig({
         rollupOptions:{
             input:{
             index: "/index.html",
-            sala_juan: "/views/sala_juan.html"
+            sala_alba:"/views/sala_alba.html",
+            sala_deep:"/views/sala_deep.html",
+            sala_elsa:"/views/sala_elsa.html",
+            sala_faryal:"/views/sala_faryal.html",
+            sala_hassan:"/views/sala_hassan.html",
+            sala_jiale:"/views/sala_jiale.html",
+            sala_jorge:"/views/sala_jorge.html",
+            sala_juan:"/views/sala_juan.html",
+            sala_luisa:"/views/sala_luisa.html",
+            sala_moha:"/views/sala_moha.html",
+            sala_paola:"/views/sala_paola.html",
         }}
 
     }
