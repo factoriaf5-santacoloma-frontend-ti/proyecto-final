c00dfbc987d787806b3d29b8c0b8e689a5088161
hassanbarag <hassanbarag100@gmail.com>
2024-12-10 13:54:34 +0100

    clickble

--- src/js/sala_hassan.js
+++ src/js/sala_hassan.js
@@ -1,24 +1,6 @@
 import AFRAME from 'aframe';
 import 'aframe-extras';
 
-AFRAME.registerComponent("play",{
-    init: function(){
-        const myEl = document.querySelector("#sound-car");
-        this.el.addEventListener("click", function (){
-            myEl.components.sound.playSound();
-        });
-    }
-    });
-    
-    
-    AFRAME.registerComponent("stop",{
-        init: function(){
-            const myEl = document.querySelector("#sound-car");
-            this.el.addEventListener("click", function (){
-                myEl.components.sound.stopSound();
-            });
-        }
-        });
 
         const cameraRig = document.querySelector("#cameraRig");
 
