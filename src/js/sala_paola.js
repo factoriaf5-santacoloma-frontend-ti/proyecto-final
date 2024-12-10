63e49260242a69e0bcc013154abc2f6b47b7ce02
Raul Garcia <raul.garcia@factoriaf5.org>
2024-12-10 14:00:41 +0100

    solve rebase conflict

---
+++ src/js/sala_paola.js
@@ -0,0 +1,7 @@
+// import AFRAME from 'aframe;
+
+// document.querySelector('a-scene').addEventListener('loaded', function () {
+//     // Inicia la animación del banner
+//     const banner = document.querySelector('#banner-welcome');
+//     banner.setAttribute('animation', 'property: position; to: 0 1 20; dur: 2000; easing: easeInOutQuad');
+// });
