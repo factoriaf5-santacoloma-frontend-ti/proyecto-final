38f286a7f6e7eba00e1a9e51d79126ba60ea1fd8
Raul Garcia <raul.garcia@factoriaf5.org>
2024-12-11 10:21:27 +0100

    merge deep branch

--- src/js/sala_deep.js
+++ src/js/sala_deep.js
@@ -1,3 +1,6 @@
+import AFRAME from 'aframe';
+import 'aframe-environment-component';
+import 'aframe-animation-component';
 
 function openDoor() {
     const door = document.getElementById('door');
