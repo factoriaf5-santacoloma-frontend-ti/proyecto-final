ea4835ab62fe2d68f559e077ceb4529e84e82464
juan963303 <estudiantejuandiego963303944>
2024-12-12 12:57:47 +0100

    update

--- src/js/sala_elsa.js
+++ src/js/sala_elsa.js
@@ -1,1 +1,23 @@
 import AFRAME from 'aframe';
+
+AFRAME.registerComponent('toggle-text', {
+  schema: {
+    groupId: { type: 'selector' }, 
+  },
+  init: function () {
+    const el = this.el; 
+    const groupEl = this.data.groupId; 
+
+    el.addEventListener('click', () => {
+      if (groupEl) {
+        const isVisible = groupEl.getAttribute('visible') === true || groupEl.getAttribute('visible') === "true";
+        groupEl.setAttribute('visible', !isVisible);
+
+        console.log(`Grupo ahora es ${!isVisible ? 'visible' : 'oculto'}`);
+      } else {
+        console.error('No se encontró el grupo asociado.');
+      }
+    });
+  },
+});
+
