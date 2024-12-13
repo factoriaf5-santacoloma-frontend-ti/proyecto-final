5ae53b00ce47d21b4cdb9ca1daf047512b723bc2
LuMorenoM <lumoreno323@gmail.com>
2024-12-13 14:12:29 +0100

    se hace prueba con los enlaces

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,37 @@
 import AFRAME from "aframe";
 import 'aframe-extras';
 
+AFRAME.registerComponent('pause-on-hover', {
+    init: function () {
+      const carousel = this.el;
+      this.el.addEventListener('mouseenter', () => {
+        const animation = carousel.getAttribute('animation');
+        animation.pause = true; // Pausa la animación
+        carousel.setAttribute('animation', animation);
+      });
+      this.el.addEventListener('mouseleave', () => {
+        const animation = carousel.getAttribute('animation');
+        animation.pause = false; // Reanuda la animación
+        carousel.setAttribute('animation', animation);
+      });
+    }
+  });
+
+  // Redirigir a href al hacer clic en los planos
+  AFRAME.registerComponent('redirect-on-click', {
+    init: function () {
+      this.el.addEventListener('click', () => {
+        const href = this.el.getAttribute('data-href');
+        if (href) {
+          window.location.href = href;
+        }
+      });
+    }
+  });
+
+  // Asignar los componentes a las entidades correspondientes
+  document.querySelector('#carousel').setAttribute('pause-on-hover', '');
+  const planes = document.querySelectorAll('.plane');
+  planes.forEach(plane => {
+    plane.setAttribute('redirect-on-click', '');
+  });
